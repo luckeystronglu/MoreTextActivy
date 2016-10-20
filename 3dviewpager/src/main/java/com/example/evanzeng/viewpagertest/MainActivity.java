54bd864c73293e361e732876b88d8af6752a3970
luckeystronglu <903804013@qq.com>
2016-10-20 13:52:37 +0800

    用ViewPager写3D画廊

--- 3dviewpager/src/main/java/com/example/evanzeng/viewpagertest/MainActivity.java
+++ 3dviewpager/src/main/java/com/example/evanzeng/viewpagertest/MainActivity.java
@@ -19,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
 
     private int pagerWidth;
     private List<ImageView> imageViewList;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
